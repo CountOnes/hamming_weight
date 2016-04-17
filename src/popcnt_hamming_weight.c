4d472d688b273dcb3d739d3364c2f407edc2da14
Daniel Lemire <lemire@gmail.com>
2016-04-17 16:54:22 -0400

    Unrolling more...

--- src/popcnt_hamming_weight.c
+++ src/popcnt_hamming_weight.c
@@ -21,15 +21,28 @@ int popcnt_bitset64_weight(const uint64_t * input, size_t length) {
     return card;
 }
 
-
 // compute the Hamming weight of an array of 64-bit words using unrolled popcnt instructions
 int unrolled_popcnt_bitset64_weight(const uint64_t * input, size_t length) {
     int card = 0;
-    for(size_t k = 0; k < length; k+=4) {
+    size_t k =0;
+    for(; k + 7 < length; k+=8) {
         card += _mm_popcnt_u64(input[k]);
         card += _mm_popcnt_u64(input[k+1]);
         card += _mm_popcnt_u64(input[k+2]);
         card += _mm_popcnt_u64(input[k+3]);
+        card += _mm_popcnt_u64(input[k+4]);
+        card += _mm_popcnt_u64(input[k+5]);
+        card += _mm_popcnt_u64(input[k+6]);
+        card += _mm_popcnt_u64(input[k+7]);
+    }
+    for(; k + 3 < length; k+=4) {
+        card += _mm_popcnt_u64(input[k]);
+        card += _mm_popcnt_u64(input[k+1]);
+        card += _mm_popcnt_u64(input[k+2]);
+        card += _mm_popcnt_u64(input[k+3]);
+    }
+    for(; k < length; k++) {
+        card += _mm_popcnt_u64(input[k]);
     }
     return card;
 }
