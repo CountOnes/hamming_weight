b937c7252f5ee615ac304c3ef37f0f86324904ce
Daniel Lemire <lemire@gmail.com>
2016-05-02 14:59:45 -0400

    adding new header

---
+++ include/jaccard_index.h
@@ -0,0 +1,12 @@
+
+
+#ifndef INCLUDE_JACCARD_INDEX_H_
+#define INCLUDE_JACCARD_INDEX_H_
+
+
+#include "scalar_jaccard_index.h"
+#include "sse_jaccard_index.h"
+#include "popcnt_jaccard_index.h"
+
+
+#endif /* INCLUDE_JACCARD_INDEX_H_ */
