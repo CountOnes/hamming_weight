2c9a2e8ca2939fe279db592c44af6993823c32fd
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2016-12-01 17:37:40 +0100

    fix format.py

--- format.py
+++ format.py
@@ -5,20 +5,23 @@ import sys
 def format(file):
     for line in file:
         line = line.strip()
-        if ':' in line: # a measurment
-            F = line.split()
-            fun  = F[0]
-            fun  = fun[:fun.index('(')] # trim the signature
-
-            if 'error' in line:
-                print '%-50s : FAILED !!!' % fun
-            else:
-                best = float(F[2])
-                avg  = float(F[7])
-
-                print '%-50s : %6.2f %6.2f' % (fun, best, avg)
-        else:
+        try:
+            pos = line.index(':')
+        except ValueError:
             print line
+            continue
+
+        # a measurment
+        F = line[pos + 1:].split()
+        fun  = line[:line.index('(')] # trim the signature
+
+        if 'error' in line:
+            print '%-50s : FAILED !!!' % fun
+        else:
+            best = float(F[0])
+            avg  = float(F[5])
+
+            print '%-50s : %6.2f %6.2f' % (fun, best, avg)
 
 if __name__ == '__main__':
     format(sys.stdin)
